cc5af41cd343479924b10db26b40faa54f2ad33c
Akbardevops-1 <97723410+Akbardevops-1@users.noreply.github.com>
2022-08-26 18:26:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 def index():
     return jsonify({'name': 'akbar',
                     'email': 'akbar@outlook.com'})
-@app.route('/health')
-def index():
-    return jsonify(
-           status:"UP"
-    )
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
 app.run()
