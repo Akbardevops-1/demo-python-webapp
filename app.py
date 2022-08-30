b6d160cf906cc3213c5f2f5b42a30c466f81d15c
Akbardevops-1 <97723410+Akbardevops-1@users.noreply.github.com>
2022-08-30 20:10:32 +0530

    update

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 #!/user/bin/env python
 # encoding: utf-8
-import json
 from flask import Flask, jsonify
 app = Flask(__name__)
-@app.route('/demo1')
-def index():
-    return jsonify({'name': 'akbar',
-                    'email': 'akbar@outlook.com'})
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/health")
+def health():
+    return jsonify(
+          status="UP"
+    )
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-app.run()
